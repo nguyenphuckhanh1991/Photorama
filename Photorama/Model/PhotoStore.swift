2eae4d7445413dbc51737421d6bea4d30827397e
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-11-03 17:32:00 +0700

    JsonSerial

--- Photorama/Model/PhotoStore.swift
+++ Photorama/Model/PhotoStore.swift
@@ -18,12 +18,16 @@ class PhotoStore {
     func fetchInterestingPhotos() {
         let url = FlickrAPI.interestingPhotosURL
         let request = URLRequest(url: url)
+        
         let task = session.dataTask(with: request) {
             (data, response, error) -> Void in
+           
             if let jsonData = data {
-                if let jsonString = String(data: jsonData,
-                                           encoding: .utf8) {
-                    print(jsonString)
+                do {
+                    let jsonObject = try JSONSerialization.jsonObject(with: jsonData, options: [])
+                    print(jsonObject)
+                } catch let error {
+                    print("Error creating JSON object: \(error)")
                 }
             } else if let requestError = error {
                 print("Error fetching interesting photos: \(requestError)")
