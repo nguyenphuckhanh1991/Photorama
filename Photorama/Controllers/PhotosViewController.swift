fd09a8479a39f9abc531ff1df69ff0a38f0938ac
nguyen.phuc.khanh <nguyen.phuc.khanh@framgias-mac-mini.local>
2017-11-06 10:20:49 +0700

    Download and Display Image

--- Photorama/Controllers/PhotosViewController.swift
+++ Photorama/Controllers/PhotosViewController.swift
@@ -20,6 +20,9 @@ class PhotosViewController: UIViewController {
             switch photosResult {
             case let .success(photos):
                 print("Successfully found \(photos.count) photos.")
+                if let firstPhoto = photos.first {
+                    self.updateImageView(for: firstPhoto)
+                }
             case let .failure(error):
                 print("Error fetching interesting photos: \(error)")
             }
@@ -29,5 +32,18 @@ class PhotosViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: Method that will fetch the image and display it on the image view.
+    func updateImageView(for photo: Photo) {
+        store.fetchImage(for: photo) {
+            (imageResult) -> Void in
+            switch imageResult {
+            case let .success(image):
+                self.imageView.image = image
+            case let .failure(error):
+                print("Error downloading image: \(error)")
+            }
+        }
     }
 }
--- Photorama/Model/PhotoStore.swift
+++ Photorama/Model/PhotoStore.swift
@@ -59,8 +59,26 @@ class PhotoStore {
         let request = URLRequest(url: photoURL)
         
         let task = session.dataTask(with: request) { (data, response, error) in
-            
+            let result = self.processImageRequest(data: data, error: error)
+            completion(result)
         }
         task.resume()
     }
+    
+    // MARK: Method that processes the data from the web service request into an image,if possible.
+    private func processImageRequest(data: Data?, error:Error?) -> ImageResult {
+        guard
+        let imageData = data,
+            let image = UIImage(data: imageData) else {
+                
+                //Couldn't create an image
+                if data == nil {
+                    return .failure(error!)
+                } else {
+                    return .failure(PhotoError.imageCreationError)
+                }
+        }
+        return .success(image)
+    }
+    
 }
